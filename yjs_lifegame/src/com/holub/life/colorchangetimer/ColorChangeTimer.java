a8f34e3fafd1b9ff7275acf61520624959b89af3
Yang <75043852+caumannerman@users.noreply.github.com>
2022-12-07 11:12:57 +0900

    ColorChangeTimer를 싱글톤으로 만들기 위해 volatile static 변수 선언

--- yjs_lifegame/src/com/holub/life/colorchangetimer/ColorChangeTimer.java
+++ yjs_lifegame/src/com/holub/life/colorchangetimer/ColorChangeTimer.java
@@ -2,12 +2,14 @@ package com.holub.life.colorchangetimer;
 
 import java.awt.*;
 
-//싱글톤으로 구현하면 됨.
+//싱글톤으로 구현하면 됨. + 옵저버로 신호 보내기
 public class ColorChangeTimer {
+    //싱글톤으로 생성하기 위해 단 하나의 static 변수
+    private volatile static ColorChangeTimer colorChangeTimer = null;
     //이거는 없애도 될 듯?
     public static boolean changeColorTick = false;
     //색깔을 change하라고 신호를 주는 기준이 되는 단위시간
-    public static int changeUnitTime;
+    public static int changeUnitTime = 1;
     // 이 클래스 객체가 만들어진 시간
     private long makeTime;
 
