453990fb92b85d62ed67d2e17c8c7173eef8b026
Yang <75043852+caumannerman@users.noreply.github.com>
2022-12-07 11:17:53 +0900

    ColorChangeTimer를 Singleton pattern으로 구현하기 위해 생성자private, getInstance()함수 구현

--- yjs_lifegame/src/com/holub/life/colorchangetimer/ColorChangeTimer.java
+++ yjs_lifegame/src/com/holub/life/colorchangetimer/ColorChangeTimer.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 //싱글톤으로 구현하면 됨. + 옵저버로 신호 보내기
 public class ColorChangeTimer {
-    //싱글톤으로 생성하기 위해 단 하나의 static 변수
+    // (#1)싱글톤으로 생성하기 위해 단 하나의 static 변수
     private volatile static ColorChangeTimer colorChangeTimer = null;
     //이거는 없애도 될 듯?
     public static boolean changeColorTick = false;
@@ -13,8 +13,20 @@ public class ColorChangeTimer {
     // 이 클래스 객체가 만들어진 시간
     private long makeTime;
 
-    public ColorChangeTimer(){
+    // (#2)싱글톤으로 생성하기 위해 생성자를 private로
+    private ColorChangeTimer(){
         this.makeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+    }
+
+    // (#3)싱글톤으로 생성하기 위해 static 객체를 가져오는 함수 구현
+    public static ColorChangeTimer getInstance(){
+        if (colorChangeTimer == null){
+            synchronized(ColorChangeTimer.class){
+                if(colorChangeTimer == null)
+                    colorChangeTimer = new ColorChangeTimer();
+            }
+        }
+        return colorChangeTimer;
     }
 
 
