b1b16d254f13e05d533cb5f01f52defad50e73b2
Caumannerman <uigohiiming2@gmail.com>
2022-12-09 03:50:12 +0900

    yellowCellColorTest수행

---
+++ yjs_lifegame/src/test/YellowCellColorTest.java
@@ -0,0 +1,38 @@
+import com.holub.life.cellcolor.CellColor;
+import com.holub.life.cellcolor.YellowCellColor;
+import com.holub.ui.Colors;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YellowCellColorTest {
+    @Test
+    void getBORDER_COLOR() {
+        CellColor yellowCellColor = new YellowCellColor();
+        Color result = yellowCellColor.getBORDER_COLOR();
+        assertEquals(Colors.DARK_YELLOW, result);
+    }
+
+    @Test
+    void getDEAD_COLOR() {
+        CellColor yellowCellColor = new YellowCellColor();
+        Color result = yellowCellColor.getDEAD_COLOR();
+        assertEquals(Colors.LIGHT_YELLOW, result);
+    }
+
+    @Test
+    void getLIVE_COLOR() {
+        CellColor yellowCellColor = new YellowCellColor();
+        Color result = yellowCellColor.getLIVE_COLOR();
+        assertEquals(Colors.OCHRE, result);
+    }
+
+    @Test
+    void getNOW_ALIVED_COLOR() {
+        CellColor yellowCellColor = new YellowCellColor();
+        Color result = yellowCellColor.getNOW_ALIVED_COLOR();
+        assertEquals(Colors.LIGHT_ORANGE, result);
+    }
+}
